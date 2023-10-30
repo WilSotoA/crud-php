d44bcb6e536a25cc295f0e5054a95627b0a4a033
WilSotoA <113364120+WilSotoA@users.noreply.github.com>
2023-10-30 15:41:47 -0500

    add-tailwind-and-include-files

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
-<?php 
+<?php
 
 require_once 'config/app.php';
 require_once 'autoload.php';
+require_once 'app/views/inc/session_start.php';
 
+
+if (isset($_GET['views'])) {
+    $url = explode('/', $_GET['views']);
+} else {
+    $url = ['login'];
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <?php require_once 'app/views/inc/head.php'; ?>
+</head>
+
+<body>
+    <?php require_once 'app/views/inc/script.php'; ?>
+    <div class="bg-red-800">hola mundo</div>
+</body>
+
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/views/content/*.{html,js,php}', './index.php'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
