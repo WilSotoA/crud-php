bd0e3f3ddc37afcff41cdbd472015a6f735aad4f
WilSotoA <113364120+WilSotoA@users.noreply.github.com>
2023-11-01 15:58:30 -0500

    implement-clean-string-method

--- app/models/mainModel.php
+++ app/models/mainModel.php
@@ -26,4 +26,28 @@ class mainModel
         $connection->exec("SET CHARACTER SET utf8");
         return $connection;
     }
+
+    protected function executeQuery($query)
+    {
+        $sql = $this->connect()->prepare($query);
+        $sql->execute();
+        return $sql;
+    }
+
+    public function cleanString($string)
+    {
+        $words = ["<script>", "</script>", "<script src", "<script type=", "SELECT * FROM", "SELECT ", " SELECT ", "DELETE FROM", "INSERT INTO", "DROP TABLE", "DROP DATABASE", "TRUNCATE TABLE", "SHOW TABLES", "SHOW DATABASES", "<?php", "?>", "--", "^", "<", ">", "==", "=", ";", "::"];
+
+        $string = trim($string);
+        $string = stripslashes($string);
+
+        foreach ($words as $word) {
+            $string = str_ireplace($word, "", $string);
+        }
+
+        $string = trim($string);
+        $string = stripslashes($string);
+
+        return $string;
+    }
 }
